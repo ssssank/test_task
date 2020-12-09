3d54d5b36f553b0c3b7bb38a52caad7392f95047
Alex Litvinov <ssssank@gmail.com>
2020-12-09 16:29:48 +0300

    Use state

--- Task_1.js
+++ Task_1.js
@@ -2,15 +2,18 @@ const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const paragraph = document.getElementById('paragraph');
 
+let canChange = false;
+
 const firstClickHandler = () => {
-  const secondClickHandler = () => {
-    paragraph.hidden = paragraph.hidden === false ? true : false;
-    button2.removeEventListener('click', secondClickHandler);
-  }
-  
-  button2.addEventListener('click', secondClickHandler);
+  canChange = true; 
 }
 
+const secondClickHandler = () => {
+  if (canChange) {
+    paragraph.hidden = paragraph.hidden === false ? true : false;
+  }
+  canChange = false;
+}
 
-
-button1.addEventListener('click', firstClickHandler)
+button1.addEventListener('click', firstClickHandler);
+button2.addEventListener('click', secondClickHandler)
