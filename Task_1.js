9abc6450f20ba6ec9dafbb30f85c0ba00ebd597c
Alex Litvinov <ssssank@gmail.com>
2020-12-09 16:20:07 +0300

    Move listners

--- Task_1.js
+++ Task_1.js
@@ -2,13 +2,15 @@ const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const paragraph = document.getElementById('paragraph');
 
-secondClickHandler = () => {
-  paragraph.hidden = paragraph.hidden === false ? true : false;
-  button2.removeEventListener('click', secondClickHandler);
-}
-
-firstClickHandler = () => {
+const firstClickHandler = () => {
+  const secondClickHandler = () => {
+    paragraph.hidden = paragraph.hidden === false ? true : false;
+    button2.removeEventListener('click', secondClickHandler);
+  }
+  
   button2.addEventListener('click', secondClickHandler);
 }
+
+
 
 button1.addEventListener('click', firstClickHandler)
